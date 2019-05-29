968bd6e775eee021784005c479387d65e661f78b
Iskra B <ibatistapoblete@gmail.com>
2019-05-29 05:16:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,6 +25,12 @@ class Scraper
   end
 
   def make_courses
+    self.get_course.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
 
   end
 
